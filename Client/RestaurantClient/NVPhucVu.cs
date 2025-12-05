fe6b1d38867d6a7383a99d39b9ff01164380df81
Troung <truongnq@suctremmt.com>
2025-12-05 22:02:55 +0700

    update more color

--- Client/RestaurantClient/NVPhucVu.cs
+++ Client/RestaurantClient/NVPhucVu.cs
@@ -175,6 +175,7 @@ namespace RestaurantClient
                             case "ChoXacNhan":
                                 row.Group = lv_ChiTietDon.Groups["ChoXacNhan"];
                                 row.ForeColor = Color.Gray;
+                                row.BackColor = Color.LightGray; // Nền xám nhạt
                                 break;
 
                             case "CoVanDe":
